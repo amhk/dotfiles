55a140ac1e176a7801e63ad240bb4728d2a520aa
Mårten Kongstad <marten.kongstad@gmail.com>
2024-01-05 16:29:33 +0100

    nvim: configure leap.vim
    Use custom keybindings in order to retain the default behaviour of 's'
    in normal mode.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- .config/nvim/lua/amhk/plugins/leap.lua
+++ .config/nvim/lua/amhk/plugins/leap.lua
@@ -3,9 +3,11 @@ return {
 	config = function()
 		local leap = require("leap")
 		leap.setup({})
-		-- leap.add_default_mappings()
-
-		-- local keymap = vim.keymap
-		-- keymap.set("n", "§", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		-- don't call leap.add_default_mappings() because it will hijack 's'
+		-- which I still use; instead add custom keybindings
+		--
+		-- see :h leap-custom-mappings for more info
+		vim.keymap.set({"n", "x", "o"}, "<leader>s", "<Plug>(leap-forward)")
+		vim.keymap.set({"n", "x", "o"}, "<leader>S", "<Plug>(leap-backward)")
 	end,
 }
